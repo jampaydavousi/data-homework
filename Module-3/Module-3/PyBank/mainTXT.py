d0f7fdc15ec636f38e71ebcc0e2aaf490dea2023
Jam Paydavousi <readsdaily@yahoo.com>
2019-06-11 14:19:41 -0700

    Add files via upload

---
+++ Module-3/Module-3/PyBank/mainTXT.py
@@ -0,0 +1,83 @@
+# import modules
+import os, csv
+
+# create and set file path to the csv file where the data resides
+csv_path = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
+#print(csv_path)
+
+# point to, open, 
+with open(csv_path, newline="", encoding="UTF8") as csv_file:
+# and read the data    
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    #print(csv_reader)
+    
+    csv_header = next(csv_reader)
+    #print(csv_header)
+
+    #initializing values before the for loop begins
+    row_count = 0
+    net = 0
+    delta = 0
+    difference = []
+    max_change = 0
+    min_change = 0
+
+ # reading and counting up the months at each row
+    for row in csv_reader:
+        # printing the elements of each each row.  Each row is a list
+        #print(row)
+        # printing the elements of each row without the single quotes
+        #print(f"{row[0]} {row[1]}")
+        row_count = row_count+1
+        net = net + int(row[1])
+        
+        #calculate the change in Profit/Loss between current and previous rows
+        change = int(row[1]) - delta
+        delta = int(row[1])
+        #put each calculated result into a new list
+        difference.append(change)
+
+        # Record the highest and lowest changes
+        if max_change < change:
+            max_change = change
+            max_month = str(row[0])    
+
+        if min_change > change:
+            min_change = change
+            min_month = str(row[0])    
+    
+    # eliminate the first record in the new list as it is the start of the period
+    difference.pop(0)   
+    # calcualte the total and quantity in order to calculate the average  
+    sum_difference = sum(difference)
+    len_difference = len(difference)
+    # calculate the average
+    avg_difference = sum(difference)/len(difference)
+    
+    # print the results to the terminal
+    print("Financial Analysis")
+    print("-" * 25)
+    print(f"Total Months: {row_count}")    
+    print(f"Total: ${net}") 
+    # only show 2 decimal points when printing the average change
+    print(f"Average Change: ${avg_difference:.2f}")
+    print(f"Greatest Increase in Profits: {max_month}  (${max_change})")  
+    print(f"Greatest Decrease in Profits: {min_month} (${min_change})")
+
+    # define lists for gathering summarized information to be exported to a new csv file
+    parameters = ["Total Months:", "Total:", "Greatest Increase in Profits:", "Greatest Decrease in Profits:"]
+    months = [" "," ", max_month, min_month]
+    values = [row_count, net, max_change, min_change]
+    
+    #zip the lists into one new list called 'results'
+    results = zip(parameters, months, values)
+    
+    # output path and new csv file name called 'summary' no need to spesify path as the destiantion is PyBank folder
+    output_file = os.path.join("summary.txt")
+    
+# open and write
+with open(output_file, "w") as data_file:
+    text_writer = csv.writer(data_file)
+
+    # write data
+    text_writer.writerows(results)
---
+++ Module-3/Module-3/PyPoll/mainTXT.py
@@ -0,0 +1,97 @@
+# import modules
+import os, csv
+
+# create and set file path to the csv file where the data resides
+csv_path = os.path.join('..', 'PyPoll', 'Resources', 'election_data.csv')
+#print(csv_path)
+
+# point to and open
+with open(csv_path, newline="", encoding="UTF8") as csv_file:
+# and read the data    
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    #print(csv_reader)
+    
+    csv_header = next(csv_reader)
+    #print(csv_header)
+
+    #initializing values before the for loop begins
+    row_count = 0
+    khan_count = 0
+    correy_count = 0
+    li_count = 0
+    otooley_count = 0
+    winner = 0
+
+ # reading and counting up the voters at each row
+    for row in csv_reader:
+        # printing the elements of each each row.  Each row is a list
+        #print(row)
+        # printing the elements of each row without the single quotes
+        #print(f"{row[0]} {row[1]}")
+        row_count = row_count+1
+        
+        if row[2] == "Khan":
+            khan_count = khan_count+1
+            
+        if  row[2] == "Correy":
+            correy_count = correy_count+1
+
+        if  row[2] == "Li":
+            li_count = li_count+1    
+
+        if  row[2] == "O'Tooley":
+            otooley_count = otooley_count+1        
+               
+    khan_pct = khan_count/row_count
+    correy_pct = correy_count/row_count
+    li_pct = li_count/row_count
+    otooley_pct = otooley_count/row_count
+  
+    print("Election Results")
+    print("-" * 60)
+    print(f"Total Votes: {row_count}")    
+    print("-" * 60)
+    print(f"Khan: {khan_pct:0.3%} ({khan_count})")
+    print(f"Correy: {correy_pct:0.3%} ({correy_count})")
+    print(f"Li: {li_pct:0.3%} ({li_count})")
+    print(f"O'Tooley: {otooley_pct:0.3%} ({otooley_count})")
+    print("-" * 60)
+# define lists for gathering summarized information to be exported to a new csv file
+    candidates = ["Khan", "Correy", "Li", "O'Tooley", "Winner Name"]
+    percentages = [khan_pct, correy_pct, li_pct, otooley_pct, winner]
+    votes = [khan_count, correy_count, li_count, otooley_count, " "]
+    
+    winner =max(percentages)
+    if winner == khan_pct:  
+        print(f"Winner: {candidates[0]}")
+        winner_name = "Khan"
+    if winner == correy_pct:  
+        print(f"Winner: {candidates[1]}")
+        winner_name = "Correy"
+    if winner == li_pct:  
+        print(f"Winner: {candidates[2]}")  
+        winner_name = "Li"
+    if winner == otooley_pct:  
+        print(f"Winner: {candidates[3]}") 
+        winner_name = "O'Tooley"
+
+ #zip the lists into one new list called 'results'
+    results = zip(candidates, percentages, votes)
+
+    # output path and new csv file name called 'summary' no need to spesify path as the destiantion is PyBank folder
+    output_file = os.path.join("summary.txt")
+    
+# open and write
+with open(output_file, "w") as data_file:
+    text_writer = csv.writer(data_file)
+
+ # write in the new header
+    text_writer.writerow(["Candidate", "Percentage of Votes", "Number of Votes"])
+
+    # write details
+    text_writer.writerows(results)
+
+    # write winner
+    text_writer.writerow(str(winner_name))
+
+
